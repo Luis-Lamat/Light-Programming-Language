6743654c180038c31ab59c0b5248fcee2b75392d
Luis-Lamat <beto_alaska@hotmail.com>
2016-03-10 16:24:11 -0600

    Successful separation of files, adds reserved words hash table

--- Code/light_scanner.py
+++ Code/light_scanner.py
@@ -1,5 +1,64 @@
 import ply.lex as lex
 
+reserved_words = {
+    # Basic tokens
+    'program'   : 'PROGRAM',
+    'light'     : 'LIGHT_TOKEN',
+    'var'       : 'VAR',
+    'print'     : 'PRINT',
+
+    # Function tokens
+    'function'  : 'FUNCTION',
+    'returns'   : 'RETURNS',
+    'return'    : 'RETURN',
+
+    # Cycle tokens
+    'loop'      : 'LOOP',
+    'for'       : 'FOR',
+    'foreach'  : 'FOR_EACH',
+    'in'        : 'IN',
+
+    # Conditional tokens
+    'if'        : 'IF',
+    'elsif'     : 'ELSIF',
+    'else'      : 'ELSE',
+
+    # Primitive type declaration tokens
+    'boolean'   : 'BOOLEAN',
+    'int'       : 'INT',
+    'decimal'   : 'DECIMAL',
+    'string'    : 'STRING',
+    'fraction'  : 'FRACTION',
+
+    # Figure type declaration tokens
+    'point'     : 'POINT',
+    'line'      : 'LINE',
+    'triangle'  : 'TRIANGLE',
+    'square'    : 'SQUARE',
+    'rectangle' : 'RECTANGLE',
+    'polygon'   : 'POLYGON',
+    'star'      : 'STAR',
+    'circle'    : 'CIRCLE',
+
+    # Figure actions tokens
+    'action'    : 'ACTION',
+    'do'        : 'DO',
+    'end'       : 'END',
+    'begins'    : 'BEGINS',
+    'ends'      : 'ENDS',
+    'move'      : 'MOVE',
+    'posX'      : 'POS_X',
+    'posY'      : 'POS_Y',
+    'scale'     : 'SCALE',
+    'size'      : 'SIZE',
+    'hide'      : 'HIDE',
+    'show'      : 'SHOW',
+    'camera'    : 'CAMERA',
+    'has'       : 'HAS',
+    'color'     : 'COLOR',
+    'angle'     : 'ANGLE'
+}
+
 tokens = (
     'PROGRAM', 'VAR_IDENTIFIER' , 'SEP_LCBRACKET' , 'SEP_RCBRACKET',
     'FUNCTION', 'LIGHT_TOKEN', 'VAR_BOOLEAN', 'VAR_INT', 'VAR_DECIMAL',
@@ -12,17 +71,10 @@ tokens = (
     'OP_GREATER_EQUAL', 'OP_LESS_EQUAL', 'OP_PLUS', 'OP_MINUS', 'OP_TIMES',
     'OP_DIVISION', 'OP_PLUS_EQUALS', 'OP_MINUS_EQUALS', 'IF', 'ELSIF', 'ELSE', 'HAS', 'COLOR',
     'SEP_HASHTAG', 'VAR', 'PRINT' , 'VAR_ANYCHAR' ,'FOR', 'ANGLE', 'VAR_VECTORID',
-    'RETURN', 
-    )
+    'RETURN' 
+)
 
-# SEPARATORS
 
-#t_SEP_HASHTAG = r'\#'
-
-# TOKENS
-t_MOVE              = r'move'
-
-#dot      = r'\.'
 def t_SEP_DOT(t):
     r'\.'
     return t
@@ -47,10 +99,6 @@ def t_SEP_SEMICOLON(t):
     ';'
     return t
 
-def t_FUNCTION (t):
-    r'function'
-    return t
-
 def t_SEP_LCBRACKET (t):
     r'{'
     return t
@@ -59,102 +107,8 @@ def t_SEP_RCBRACKET (t):
     r'}'
     return t
 
-def t_LIGHT_TOKEN(t):
-    r'light'
-    return t
-
-def t_RETURNS (t):
-    r'returns'
-    return t
-
-def t_RETURN (t):
-    r'return'
-    return t
-
-def t_VAR (t):
-    r'var\s'
-    return t
-
-def t_DECIMAL (t):
-    r'decimal'
-    return t
-
-def t_STRING (t):
-    r'string'
-    return t
-
-def t_PRINT (t):
-    r'print'
-    return t
-
 def t_VAR_VECTORID (t):
     r'v[0-9]+'
-    return t
-
-def t_ANGLE (t):
-    r'angle'
-    return t
-
-def t_FOR_EACH (t):
-    r'for_each'
-    return t
-
-def t_FOR (t):
-    r'for'
-    return t
-    
-def t_ACTION (t):
-    r'action'
-    return t
-
-def t_DO (t):
-    r'do'
-    return t
-
-def t_BEGINS (t):
-    r'begins'
-    return t
-
-def t_ENDS (t):
-    r'ends'
-    return t
-def t_MOVE (t):
-    r'pos_x'
-    return t
-def t_POS_Y (t):
-    r'pos_y'
-    return t
-
-def t_END (t):
-    r'end'
-    return t
-  
-def t_HAS (t):
-    r'has'
-    return t
-
-def t_COLOR (t):
-    r'color'
-    return t
-
-def t_SCALE (t):
-    r'scale'
-    return t
-
-def t_SIZE (t) : 
-    r'size'
-    return t
-
-def t_HIDE (t) : 
-    r'hide'
-    return t
-
-def t_SHOW (t) : 
-    r'show'
-    return t
-
-def t_CAMERA (t) : 
-    r'camera'
     return t
 
 def t_OP_LESS_THAN (t) : 
@@ -217,81 +171,14 @@ def t_OP_NOT_EQUAL(t):
     r'!='
     return t
 
-def t_PROGRAM (t):
-    r'program'
-    return t
-
-def t_IF (t):
-    r'if'
-    return t
-
-def t_ELSIF (t):
-    r'elsif'
-    return t
-
-def t_ELSE (t):
-    r'else'
-    return t
-
-def t_PRINT (t):
-    r'print'
-    return t
-
-def t_INT (t):
-    r'int'
-    return t
-
-def t_FLOAT (t):
-    r'float'
-    return t
-
-def t_LOOP (t):
-    r'loop'
-    return t
-
-def t_IN (t):
-    r'in\s'
-    return t
-
 #Figures
-
-def t_STAR (t):
-    r'STAR'
-    return t
-
-def t_POLYGON (t):
-    r'polygon'
-    return t
-
-def t_POINT (t):
-    r'point'
-    return t
-
-def t_LINE (t):
-    r'line'
-    return t
-
-def t_TRIANGLE (t):
-    r'triangle'
-    return t
-
-def t_CIRCLE (t):
-    r'circle'
-    return t
-
-def t_SQUARE (t):
-    r'square'
-    return t
-
-def t_RECTANGLE (t):
-    r'rectangle'
-    return t
 
 def t_VAR_FRACTION (t):
     r'[0-9]+[\\][0-9]+'
 
 def t_VAR_IDENTIFIER (t):
     r'[a-zA-Z][a-zA-Z0-9]*'
+    t.type = reserved_words.get(t.value, 'VAR_IDENTIFIER')
     return t
 
 def t_VAR_DECIMAL (t):
--- Code/run_script.sh
+++ Code/run_script.sh
@@ -1,2 +1,2 @@
 echo "\n[$(date)] =========================================== " >> latest_log.txt
-python light_scanner_parser.py input.in  2>&1 | tee -a latest_log.txt
+python light_parser.py input.in  2>&1 | tee -a latest_log.txt
