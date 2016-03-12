bd814719a34443322eaadee1f31f7ccd969eaa5a
Enrique Hernandez <enrique@axented.com>
2016-03-12 17:36:00 -0600

    fix number representation

--- Code/light_semantic_controller.py
+++ Code/light_semantic_controller.py
@@ -4,11 +4,11 @@ from ligt_datastructures import *
 #INITIALIZE DICTIONARIES
 
 type_dict = {
-	'boolean'   : '0',
-    'int'       : '1',
-    'decimal'   : '2',
-    'string'    : '3',
-    'fraction'  : '4'
+	'boolean'   : 0,
+    'int'       : 1,
+    'decimal'   : 2,
+    'string'    : 3,
+    'fraction'  : 4
 }
 
 var_dict = {
@@ -44,16 +44,16 @@ gl_info = Global_information()
 
 
 #DICTIONARY FUNCTIONS
-def add_var_dic(var_name, var_type, var_value,var_function):
-	if var_function == 'light':
+def add_var_dic(var_name, var_type, var_value, var_function):
+	if var_function == '':
 		if !var_exists_light(var_name):
-			var_dict['light'][var_name] = Var(gl_info.get_next_var_id, var_name,'global', type_dict[var_type], var_value)
+			var_dict['global'][var_name] = Var(gl_info.get_next_var_id, var_name,'global', type_dict[var_type], var_value)
 			return True
 		else:
 			return False
 	else:
 		if !var_exists_func(var_name, var_function):
-			var_dict['global'][var_function][var_name] = Var(gl_info.get_next_var_id, var_name,'local', type_dict[var_type], var_value)
+			var_dict['local'][var_function][var_name] = Var(gl_info.get_next_var_id, var_name,'local', type_dict[var_type], var_value)
 
 def var_exists_light(var_name):
 	if var_name in var_dict['global']:
