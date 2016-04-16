3562fd09b1a5ce9895c0c0747742edaf86973773
Enrique Hernandez <enrique@axented.com>
2016-04-15 21:58:57 -0500

    Memory handler arithmetic operators, assign operator working

--- Code/memory/memory_handler.py
+++ Code/memory/memory_handler.py
@@ -1,6 +1,10 @@
 from light_datastructures import *
 from light_semantic_controller import *
 from memory import *
+import operator
+
+
+
 
 class MemoryHandler(object):
 	"""MemoryHandler class"""
@@ -15,5 +19,25 @@ class MemoryHandler(object):
 	def __init__(cls):
 		cls.__dict__ = cls.__shared_state
 
+	def arithmetic_operator(cls, quad, index):
+		left_op = cls.get_address_value(quad.left_operand)
+		right_op = cls.get_address_value(quad.right_operand)
+		result = execute_arithmetic_operator(quad.operator, left_op, right_op)
+		cls.set_address_value(quad.result, result)
 
-		
+	def execute_arithmetic_operator(cls, val, x, y):
+		ops = {
+
+			0	: operator.add(x,y),
+			1	: operator.sub(x,y),
+			2	: operator.mul(x,y),
+			3	: operator.div(x,y)
+		}
+		return ops[val]
+
+	def assign_operator(cls, quad, index):
+		from_value = cls.get_address_value(quad.left_operand)
+		cls.set_address_value(quad.result, from_value)
+
+
+
--- Code/virtual_machine.py
+++ Code/virtual_machine.py
@@ -2,123 +2,123 @@ from light_datastructures import *
 from quadruple import *
 
 
+def execute_operator(argument, quad, index):
+	switcher = {
+		0	: 	plus,
+		1	: 	minus,
+		2	: 	times,
+		3	:	over,
+		4	:	lessThan,
+		5	:	greaterThan,
+		6	:	lessEqThan,
+		7	: 	greaterEqThan,
+		8	:	equals,
+		9	:	notEqual,
+		10	:	_and,
+		11	:	_or,
+		12	:	lPar,
+		13	:	rPar,
+		14	:	equal
+		15	:	gotof,
+		16	:	gotot,
+		17	:	goto,
+		18	:	ret,
+		19	:	_return,
+		20	:	gosub,
+		21	:	era,
+		22	:	param,
+		23	:	_print,
+		24	:	end,	
+
+	}
+	# Get the function from switcher dictionary
+	func = switcher.get(argument, lambda: "nothing")
+	# Execute the function
+	return func(quad, index)
+
+
+def plus(quad, index):
+	pass
+
+def minus(quad, index):
+	pass
+
+def times(quad, index):
+	pass
+
+def over(quad, index):
+	pass
+
+def lessThan(quad, index):
+	pass
+
+def greaterThan(quad, index):
+	pass
+
+def lessEqThan(quad, index):
+	pass
+
+def greaterEqThan(quad, index):
+	pass
+
+def equals(quad, index):
+	pass
+
+def notEqual(quad, index):
+	pass
+
+def _and(quad, index):
+	pass
+
+def _or(quad, index):
+	pass
+
+def lPar(quad, index):
+	pass
+
+def rPar(quad, index):
+	pass
+
+def equal(quad, index):
+	pass
+
+def gotof(quad, index):
+	pass
+
+def gotof(quad, index):
+	pass
+
+def goto(quad, index):
+	pass
+
+def ret(quad, index):
+	pass
+
+def _return(quad, index):
+	pass
+
+def gosub(quad, index):
+	pass
+
+def era(quad, index):
+	pass
+
+def param(quad, index):
+	pass
+
+def _print(quad, index):
+	pass
+
+def end(quad, index):
+	pass
+
+
+
+
 quads = Quadruples.quad_list
-iteratorValue = 0
 
 for i in xrange(len(quads)):
 
 	op = quads[i].operator
-	iteratorValue = i
-	execute_operator(op)
-
-
-def plus():
-    pass
-
-def minus():
-	pass
-
-def times():
-	pass
-
-def over():
-	pass
-
-def lessThan():
-	pass
-
-def greaterThan():
-	pass
-
-def lessEqThan():
-	pass
-
-def greaterEqThan():
-	pass
-
-def equals():
-	pass
-
-def notEqual():
-	pass
-
-def _and():
-	pass
-
-def _or():
-	pass
-
-def lPar():
-	pass
-
-def rPar():
-	pass
-
-def equal():
-	pass
-
-def gotof():
-	pass
-
-def gotof():
-	pass
-
-def goto():
-	pass
-
-def ret():
-	pass
-
-def _return():
-	pass
-
-def gosub():
-	pass
-
-def era():
-	pass
-
-def param():
-	pass
-
-def _print():
-	pass
-
-def end():
-	pass
-
-
-def execute_operator(argument):
-    switcher = {
-        0	: 	plus,
-        1	: 	minus,
-        2	: 	times,
-        3	:	over,
-        4	:	lessThan,
-        5	:	greaterThan,
-        6	:	lessEqThan,
-        7	: 	greaterEqThan,
-        8	:	equals,
-        9	:	notEqual,
-        10	:	_and,
-        11	:	_or,
-        12	:	lPar,
-        13	:	rPar,
-        14	:	equal
-        15	:	gotof,
-        16	:	gotot,
-        17	:	goto,
-        18	:	ret,
-        19	:	_return,
-        20	:	gosub,
-        21	:	era,
-        22	:	param,
-        23	:	_print,
-        24	:	end,	
-
-    }
-    # Get the function from switcher dictionary
-    func = switcher.get(argument, lambda: "nothing")
-    # Execute the function
-    return func()
+	execute_operator(op, quads[i], i)
 
