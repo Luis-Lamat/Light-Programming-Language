3a754f8c092b23a6f057c1d5ae0e2aecace0958b
Luis-Lamat <beto_alaska@hotmail.com>
2016-04-15 21:55:17 -0500

    Adds get and set addres value in memory

--- Code/memory/memory_handler.py
+++ Code/memory/memory_handler.py
@@ -16,5 +16,23 @@ class MemoryHandler(object):
 	def __init__(cls):
 		cls.__dict__ = cls.__shared_state
 
+	@classmethod
+	def get_address_value(cls, addr):
+		type = (addr // 1000) # integer division
+		relative_address = addr - type
+		# use heap for search if addr is negative, else the current local mem
+		heap_or_stack = cls.heap if addr < 0 else cls.stack.peek()
+		return heap_or_stack.memory[type][abs(relative_address)]
+
+	@classmethod
+	def set_address_value(cls, addr, val):
+		type = (addr // 1000) # integer division
+		relative_address = addr - type
+		# use heap for search if addr is negative, else the current local mem
+		if addr < 0:
+			cls.heap[type][relative_address] = val
+		else:
+			cls.stack.peek().[type][relative_address] = val
+
 
 		
