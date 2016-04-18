24bde185894bc18c2e8bbb51a539f2a29347db9e
Luis-Lamat <beto_alaska@hotmail.com>
2016-04-18 16:18:38 -0500

    Printing something in memory.py

--- Code/memory.py
+++ Code/memory.py
@@ -5,6 +5,7 @@ class Memory(object):
 	# @param size (num): the amount of types it will hold
 	# @param sublists_sizes (list(num)): the amount of variables in each type
 	def __init__(self, size, sublists_sizes):
+		print "> NEW MEMORY: Size = {}, Sublist Sizes = {}".format(size, sublists_sizes)
 		self.return_address = None
 		self.memory = [[] for x in xrange(size)]
 		for i in xrange(size):
